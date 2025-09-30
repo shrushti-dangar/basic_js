a5d98a4501b452ca5185bd7f0336353483e9df56
shrushti-dangar <shrushtidangar18@gmail.com>
2025-09-30 13:00:40 +0530

    variable and datatypes

---
+++ 01_datatypes.js
@@ -0,0 +1,19 @@
+let state;   //undefine avse output..
+
+//datatypes:-
+
+//number => 2 to 53 range
+//bigint use kari sakay
+//string =>""
+//boolean => true/false
+//null =>emty 
+//undefined=> 
+//symbol =>unique
+
+
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
---
+++ 01_variables.js
@@ -0,0 +1,34 @@
+const accountId =144553  //const ma fix value thay jase je change ny thay
+let accountEmail ="shrushti@gmail.com"  //let better che badha karta 
+var accountPassword ="12223"  //scope problem avi sake che 
+accountCity ="jaipur"  //amnm bhi declare kari sakay che
+let accountstate;  //value nathi api atle undefine avse...
+
+//accountId = 2   const ma fix value thay jase je change ny thay.....
+
+
+//change thay sakse:-
+accountEmail ="@shrushti"
+accountPassword ="shrushti"
+accountCity="rajkot"
+
+
+
+console.log("accountId");
+console.table([accountId,accountEmail ,accountPassword,  accountCity]);  //console.table thi multiple value print thay sakse...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
